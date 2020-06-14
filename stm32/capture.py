a64aaf13d33ae43fd69534adb5ff8918cc910d49
root <github@danman.eu>
2020-06-14 17:20:02 +0200

    capture bug fixing

--- stm32/capture.py
+++ stm32/capture.py
@@ -3,25 +3,23 @@
 import serial
 import time
 
-BUFFER_SIZE = 1024
-
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
 message = b""
 
+print("2bus capture started")
 while True:
 
     while True:
-        data = ser.read(BUFFER_SIZE)
-        if len(data) < 1:
-            print("no data")
+        data = ser.read(1)
+        if len(data) < 1 or data == 0xff:
             if len(message) > 0:
                 filename = "%f.bin" % (time.time())
                 print(filename)
                 f = open(filename, "bw")
                 f.write(message)
                 f.close()
-                message = ""
+                message = b""
             continue
         print("got data",len(data))
         message += data
